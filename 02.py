989cc6a8cf2d60b0a3c5c2858790c86370f0b647
HYUN1346 <your@email.com>
2025-09-12 02:41:35 +0900

    첫 번째 커밋

---
+++ 02.py
@@ -0,0 +1,19 @@
+#import numpy as np # 패키지를 설치할때 아나콘다의 open terminal에서 pip install numpy 입력
+#data = [1,2,3]
+#arr = np.array(data)
+#print(arr)
+#print(type(arr)) 
+
+# data = [1,2,3]
+
+# result = [10]
+# for i in data:
+#     result.append(i*10)
+#     print("for result = ",result)
+
+# print("final result = ",result)
+
+import numpy as np
+arr = np.array(data)
+result = arr * 10
+print("fianal result = ",result)
